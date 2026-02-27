67874e4235b3bf84e0733c190776d87a99671dce
sreevani <sreevanidanapala@gmail.com>
2026-02-27 12:01:01 +0530

    week2 traim model

--- src/api/app.py
+++ src/api/app.py
@@ -1,36 +1,22 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from pydantic import BaseModel
-from ner.inference import extract_entities
-from postprocessing.validator import validate_output
-from ocr.ocr_engine import extract_text_from_pdf
-from ocr.text_cleaning import clean_text
-import tempfile
+from src.ner.inference import extract_entities
+from src.postprocessing.validator import validate_entities
 
 app = FastAPI(title="LexiScan Auto API")
 
-class TextRequest(BaseModel):
+
+class ContractRequest(BaseModel):
     text: str
 
 
 @app.get("/")
 def home():
-    return {"message": "LexiScan Auto Running"}
+    return {"message": "LexiScan Auto Running Successfully"}
 
 
 @app.post("/extract-entities")
-async def extract_entities_api(file: UploadFile = File(None), request: TextRequest = None):
-
-    if file:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
-            tmp.write(await file.read())
-            text = extract_text_from_pdf(tmp.name)
-    elif request:
-        text = request.text
-    else:
-        return {"error": "Provide PDF file or raw text"}
-
-    cleaned_text = clean_text(text)
-    raw_output = extract_entities(cleaned_text)
-    validated = validate_output(raw_output)
-
-    return validated
+def extract(request: ContractRequest):
+    raw_output = extract_entities(request.text)
+    validated_output = validate_entities(raw_output)
+    return validated_output
